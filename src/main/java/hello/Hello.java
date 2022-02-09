92c50b911d038514fedcaa89b89d1646fe221fd4
Alexandra Psheborovskaya <alexandra.psheborovskaya@jetbrains.com>
2022-02-09 16:44:01 +0300

    Initial commit

---
+++ src/main/java/hello/Hello.java
@@ -0,0 +1,13 @@
+package hello;
+
+import java.util.stream.Stream;
+
+
+public class Hello {
+
+    public static void main(String[] args) {
+        if (true) {
+
+        }
+    }
+}
---
+++ src/main/java/holly/Holly.java
@@ -0,0 +1,13 @@
+package holly;
+
+import java.util.stream.Stream;
+
+
+public class Holly {
+
+    public static void main(String[] args) {
+        if (true) {
+
+        }
+    }
+}
